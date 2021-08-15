6dec455dee7ff18a0c513a497928ae93eff21143
Ilya Malygin <malygin.ilja@yandex.ru>
2021-08-15 20:21:49 +0300

    fix

--- second_site/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from. import views
-from django.contrib.auth.views import LogoutView
-urlpatterns = [
-    path('login/', views.user_login, name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('register/', views.register, name='register'),
-
-]
